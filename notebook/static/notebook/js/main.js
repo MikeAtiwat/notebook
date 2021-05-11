7f803aac44e96e684dcc96cb65d533d5e6acb09c
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-11 16:22:30 +0700

    create notebook

--- notebook/static/notebook/js/main.js
+++ notebook/static/notebook/js/main.js
@@ -139,111 +139,111 @@ requirejs([
         contents: contents,
         config: config_section},
         common_options));
-    var login_widget = new loginwidget.LoginWidget('span#login_widget', common_options);
-    var toolbar = new maintoolbar.MainToolBar('#maintoolbar-container', {
-        notebook: notebook,
-        events: events,
-        actions: acts});
-    var quick_help = new quickhelp.QuickHelp({
-        keyboard_manager: keyboard_manager,
-        events: events,
-        notebook: notebook});
-    keyboard_manager.set_notebook(notebook);
-    keyboard_manager.set_quickhelp(quick_help);
-    var menubar = new menubar.MenuBar('#menubar', $.extend({
-        notebook: notebook,
-        contents: contents,
-        events: events,
-        save_widget: save_widget,
-        quick_help: quick_help,
-        actions: acts,
-        config: config_section},
-        common_options));
-    var notification_area = new notificationarea.NotebookNotificationArea(
-        '#notification_area', {
-        events: events,
-        save_widget: save_widget,
-        notebook: notebook,
-        keyboard_manager: keyboard_manager});
-    notification_area.init_notification_widgets();
-    var kernel_selector = new kernelselector.KernelSelector(
-        '#kernel_logo_widget', notebook);
-    searchandreplace.load(keyboard_manager);
+    // var login_widget = new loginwidget.LoginWidget('span#login_widget', common_options);
+    // var toolbar = new maintoolbar.MainToolBar('#maintoolbar-container', {
+    //     notebook: notebook,
+    //     events: events,
+    //     actions: acts});
+    // var quick_help = new quickhelp.QuickHelp({
+    //     keyboard_manager: keyboard_manager,
+    //     events: events,
+    //     notebook: notebook});
+    // keyboard_manager.set_notebook(notebook);
+    // keyboard_manager.set_quickhelp(quick_help);
+    // var menubar = new menubar.MenuBar('#menubar', $.extend({
+    //     notebook: notebook,
+    //     contents: contents,
+    //     events: events,
+    //     save_widget: save_widget,
+    //     quick_help: quick_help,
+    //     actions: acts,
+    //     config: config_section},
+    //     common_options));
+    // var notification_area = new notificationarea.NotebookNotificationArea(
+    //     '#notification_area', {
+    //     events: events,
+    //     save_widget: save_widget,
+    //     notebook: notebook,
+    //     keyboard_manager: keyboard_manager});
+    // notification_area.init_notification_widgets();
+    // var kernel_selector = new kernelselector.KernelSelector(
+    //     '#kernel_logo_widget', notebook);
+    // searchandreplace.load(keyboard_manager);
 
-    $('body').append('<div id="fonttest"><pre><span id="test1">x</span>'+
-                     '<span id="test2" style="font-weight: bold;">x</span>'+
-                     '<span id="test3" style="font-style: italic;">x</span></pre></div>');
-    var nh = $('#test1').innerHeight();
-    var bh = $('#test2').innerHeight();
-    var ih = $('#test3').innerHeight();
-    if(nh != bh || nh != ih) {
-        $('head').append('<style>.CodeMirror span { vertical-align: bottom; }</style>');
-    }
-    $('#fonttest').remove();
+    // $('body').append('<div id="fonttest"><pre><span id="test1">x</span>'+
+    //                  '<span id="test2" style="font-weight: bold;">x</span>'+
+    //                  '<span id="test3" style="font-style: italic;">x</span></pre></div>');
+    // var nh = $('#test1').innerHeight();
+    // var bh = $('#test2').innerHeight();
+    // var ih = $('#test3').innerHeight();
+    // if(nh != bh || nh != ih) {
+    //     $('head').append('<style>.CodeMirror span { vertical-align: bottom; }</style>');
+    // }
+    // $('#fonttest').remove();
 
-    page.show();
+    // page.show();
 
-    events.one('notebook_loaded.Notebook', function () {
-        var hash = document.location.hash;
-        if (hash) {
-            document.location.hash = '';
-            document.location.hash = hash;
-        }
-        notebook.set_autosave_interval(notebook.minimum_autosave_interval);
-    });
+    // events.one('notebook_loaded.Notebook', function () {
+    //     var hash = document.location.hash;
+    //     if (hash) {
+    //         document.location.hash = '';
+    //         document.location.hash = hash;
+    //     }
+    //     notebook.set_autosave_interval(notebook.minimum_autosave_interval);
+    // });
 
-    IPython.page = page;
-    IPython.notebook = notebook;
-    IPython.contents = contents;
-    IPython.pager = pager;
-    IPython.quick_help = quick_help;
-    IPython.login_widget = login_widget;
-    IPython.menubar = menubar;
-    IPython.toolbar = toolbar;
-    IPython.notification_area = notification_area;
-    IPython.keyboard_manager = keyboard_manager;
-    IPython.save_widget = save_widget;
-    IPython.tooltip = notebook.tooltip;
+    // IPython.page = page;
+    // IPython.notebook = notebook;
+    // IPython.contents = contents;
+    // IPython.pager = pager;
+    // IPython.quick_help = quick_help;
+    // IPython.login_widget = login_widget;
+    // IPython.menubar = menubar;
+    // IPython.toolbar = toolbar;
+    // IPython.notification_area = notification_area;
+    // IPython.keyboard_manager = keyboard_manager;
+    // IPython.save_widget = save_widget;
+    // IPython.tooltip = notebook.tooltip;
 
-    try {
-        events.trigger('app_initialized.NotebookApp');
-    } catch (e) {
-        console.error("Error in app_initialized callback", e);
-    }
+    // try {
+    //     events.trigger('app_initialized.NotebookApp');
+    // } catch (e) {
+    //     console.error("Error in app_initialized callback", e);
+    // }
 
-    Object.defineProperty( IPython, 'actions', {
-      get: function() {
-          console.warn('accessing "actions" on the global IPython/Jupyter is not recommended. Pass it to your objects constructors at creation time');
-          return acts;
-      },
-      enumerable: true,
-      configurable: false
-    });
+    // Object.defineProperty( IPython, 'actions', {
+    //   get: function() {
+    //       console.warn('accessing "actions" on the global IPython/Jupyter is not recommended. Pass it to your objects constructors at creation time');
+    //       return acts;
+    //   },
+    //   enumerable: true,
+    //   configurable: false
+    // });
 
-    clipboard.setup_clipboard_events();
+    // clipboard.setup_clipboard_events();
     
-    // Now actually load nbextensionsload_extensions_from_config
-    Promise.all([
-        utils.load_extensions_from_config(config_section),
-        utils.load_extensions_from_config(common_config),
-    ])
-    .catch(function(error) {
-        console.error('Could not load nbextensions from user config files', error);
-    })
-    // BEGIN HARDCODED WIDGETS HACK
-    .then(function() {
-        if (!utils.is_loaded('jupyter-js-widgets/extension')) {
-            // Fallback to the ipywidgets extension
-            utils.load_extension('widgets/notebook/js/extension').catch(function () {
-                console.warn('Widgets are not available.  Please install widgetsnbextension or ipywidgets 4.0');
-            });
-        }
-    })
-    .catch(function(error) {
-        console.error('Could not load ipywidgets', error);
-    });
-    // END HARDCODED WIDGETS HACK
+    // // Now actually load nbextensionsload_extensions_from_config
+    // Promise.all([
+    //     utils.load_extensions_from_config(config_section),
+    //     utils.load_extensions_from_config(common_config),
+    // ])
+    // .catch(function(error) {
+    //     console.error('Could not load nbextensions from user config files', error);
+    // })
+    // // BEGIN HARDCODED WIDGETS HACK
+    // .then(function() {
+    //     if (!utils.is_loaded('jupyter-js-widgets/extension')) {
+    //         // Fallback to the ipywidgets extension
+    //         utils.load_extension('widgets/notebook/js/extension').catch(function () {
+    //             console.warn('Widgets are not available.  Please install widgetsnbextension or ipywidgets 4.0');
+    //         });
+    //     }
+    // })
+    // .catch(function(error) {
+    //     console.error('Could not load ipywidgets', error);
+    // });
+    // // END HARDCODED WIDGETS HACK
 
-    notebook.load_notebook(common_options.notebook_path);
+    // notebook.load_notebook(common_options.notebook_path);
 
 });
