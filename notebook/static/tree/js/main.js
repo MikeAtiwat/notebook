4dc457c099f3112bdb431430789bcb9d35e2a6ab
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-11 18:05:32 +0700

    content

--- notebook/static/tree/js/main.js
+++ notebook/static/tree/js/main.js
@@ -85,13 +85,13 @@ requirejs([
 
     page = new page.Page('div#header', 'div#site');
 
-    // var session_list = new sesssionlist.SesssionList($.extend({
-    //     events: events},
-    //     common_options));
-    // var contents = new contents_service.Contents({
-    //     base_url: common_options.base_url,
-    //     common_config: common_config
-    // });
+    var session_list = new sesssionlist.SesssionList($.extend({
+        events: events},
+        common_options));
+    var contents = new contents_service.Contents({
+        base_url: common_options.base_url,
+        common_config: common_config
+    });
     // IPython.NotebookList = notebooklist.NotebookList;
     // var notebook_list = new notebooklist.NotebookList('#notebook_list', $.extend({
     //     contents: contents,
