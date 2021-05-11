379f323265ed277013d96469933f93f8f7e79463
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-11 12:48:27 +0700

    extend session list

--- notebook/static/tree/js/main.js
+++ notebook/static/tree/js/main.js
@@ -85,9 +85,9 @@ requirejs([
 
     page = new page.Page('div#header', 'div#site');
 
-    // var session_list = new sesssionlist.SesssionList($.extend({
-    //     events: events},
-    //     common_options));
+    var session_list = new sesssionlist.SesssionList($.extend({
+        events: events},
+        common_options));
     // var contents = new contents_service.Contents({
     //     base_url: common_options.base_url,
     //     common_config: common_config
