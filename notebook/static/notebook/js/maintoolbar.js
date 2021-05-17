4c27acab8af8540fc1412104a7cb4dcc251662b6
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-17 17:26:15 +0700

    api dir

--- notebook/static/notebook/js/maintoolbar.js
+++ notebook/static/notebook/js/maintoolbar.js
@@ -87,7 +87,7 @@ define([
 
     var displayMetrics = function () {
        
-      $.getJSON({url: utils.get_body_data('baseUrl')+'api/metrics/v1',
+      $.getJSON({url: utils.get_body_data('baseUrl')+'api/',
             success: function (data) {
         console.dir(data)}
       })
