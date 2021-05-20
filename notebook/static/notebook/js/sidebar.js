86b236702550c2222334d1ed840b8e8696a3135e
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-20 15:25:37 +0700

    console.log

--- notebook/static/notebook/js/sidebar.js
+++ notebook/static/notebook/js/sidebar.js
@@ -19,8 +19,9 @@ define([
 
     SideBar.prototype.bind_events = function () {
         var that = this;
-        var open_true = this.open
         this.element.find("#open_btn").click(function () {
+       	var open_true = this.open
+       	console.log(open_true)
         if(!open_true){
 			  document.getElementById("mySidebar").style.width = "250px";
 			  document.getElementById("main").style.marginLeft = "250px";
