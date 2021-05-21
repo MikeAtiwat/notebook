c287d21eeb5e0d65272eac7b8d74e7c430787d1d
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-21 13:18:23 +0700

    button sidebar

--- notebook/static/notebook/js/buttonsidebar.js
+++ notebook/static/notebook/js/buttonsidebar.js
@@ -1,41 +1,41 @@
-// Copyright (c) Jupyter Development Team.
-// Distributed under the terms of the Modified BSD License.
+// // Copyright (c) Jupyter Development Team.
+// // Distributed under the terms of the Modified BSD License.
 
-define([
-    'jquery',
-    'base/js/utils',
-], function($, utils){
-    "use strict";
+// define([
+//     'jquery',
+//     'base/js/utils',
+// ], function($, utils){
+//     "use strict";
 
-    var ButtonSideBar = function (selector) {
-        this.selector = selector;
-        this.open = false;
-        if (this.selector !== undefined) {
-            this.element = $(selector);
-            this.bind_events();
-        }
-    };
+//     var ButtonSideBar = function (selector) {
+//         this.selector = selector;
+//         this.open = false;
+//         if (this.selector !== undefined) {
+//             this.element = $(selector);
+//             this.bind_events();
+//         }
+//     };
 
 
-    SideBar.prototype.bind_events = function () {
-        var that = this;
-        this.element.find("#open_btn").click(function () {
-       	var open_true = this.open
-       	console.log(open_true)
-        if(!open_true){
-			  document.getElementById("mySidebar").style.width = "250px";
-			  document.getElementById("main").style.marginRight = "250px";
-			  this.open = true;
-    	}else{
-			  document.getElementById("mySidebar").style.width = "0";
-			  document.getElementById("main").style.marginRight = "0";
-			  this.open = false;    		
-    	}
+//     SideBar.prototype.bind_events = function () {
+//         var that = this;
+//         this.element.find("#open_btn").click(function () {
+//        	var open_true = this.open
+//        	console.log(open_true)
+//         if(!open_true){
+// 			  document.getElementById("mySidebar").style.width = "250px";
+// 			  document.getElementById("main").style.marginRight = "250px";
+// 			  this.open = true;
+//     	}else{
+// 			  document.getElementById("mySidebar").style.width = "0";
+// 			  document.getElementById("main").style.marginRight = "0";
+// 			  this.open = false;    		
+//     	}
 
     
-    });
-    };
+//     });
+//     };
 
 
-    return {'ButtonSideBar': ButtonSideBar};
-});
+//     return {'ButtonSideBar': ButtonSideBar};
+// });
