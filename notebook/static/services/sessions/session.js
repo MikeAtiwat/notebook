e242d06799b6e7130ffab2d22c0db6663d4de853
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-21 11:00:33 +0700

    no console

--- notebook/static/services/sessions/session.js
+++ notebook/static/services/sessions/session.js
@@ -104,13 +104,9 @@ define([
         var on_success = function (data, status, xhr) {
             if (that.kernel) {
                 that.kernel.name = that.kernel_model.name;
-                console.log("option1")
-                console.log(that.kernel.name)
             } else {
                 var kernel_service_url = utils.url_path_join(that.base_url, "api/kernels");
                 that.kernel = new kernel.Kernel(kernel_service_url, that.ws_url, that.kernel_model.name);
-                console.log("option2")
-                console.log(kernel_service_url)
             }
             that.events.trigger('kernel_created.Session', {session: that, kernel: that.kernel});
             that.kernel._kernel_created(data.kernel);
