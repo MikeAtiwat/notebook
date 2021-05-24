ca9bfde69cb5cf5e524ba6ea2314cd9dd63364e3
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-24 17:00:35 +0700

    500px

--- notebook/static/notebook/js/buttonsidebar.js
+++ notebook/static/notebook/js/buttonsidebar.js
@@ -23,11 +23,11 @@ define([
          var open_true = this.open
          console.log(open_true)
         if(!open_true){
-        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("mySidebar").style.width = "500px";
         // document.getElementById("main").style.marginRight = "250px";
-        document.getElementById("site").style.marginRight = "250px";
-        document.getElementById("notebook_panel").style.marginRight = "250px";
-        document.getElementById("header").style.marginRight = "250px";
+        document.getElementById("site").style.marginRight = "500px";
+        document.getElementById("notebook_panel").style.marginRight = "500px";
+        document.getElementById("header").style.marginRight = "500px";
         this.open = true;
       }else{
         document.getElementById("mySidebar").style.width = "0";
