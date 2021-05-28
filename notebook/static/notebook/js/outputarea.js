4763817e7fc2149b40b76ac838254017d5737b24
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-28 18:51:13 +0700

    edit

--- notebook/static/notebook/js/outputarea.js
+++ notebook/static/notebook/js/outputarea.js
@@ -45,7 +45,7 @@ define([
         this.bind_events();
         this.class_config = new configmod.ConfigWithDefaults(this.config,
                                         OutputArea.config_defaults, 'OutputArea');
-        this.acts = new actions.init();
+
         this.handle_appended = utils.throttle(this.handle_appended.bind(this));
     };
 
@@ -99,7 +99,8 @@ define([
         this.prompt_overlay.addClass('out_prompt_overlay prompt');
         this.prompt_overlay.attr('title', i18n.msg._('click to expand output; double click to hide output'));
         this.tc_dis.addClass("toolcell").attr('id','tool_cell');
-        this.toolbar = new toolcell.ToolCell('#tool_cell', {actions: this.acts});
+        var acts = new actions.init();
+        var toolbar = new toolcell.ToolCell('#tool_cell', {actions: acts});
         this.expand();
     };
 
