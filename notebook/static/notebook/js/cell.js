f9d818b0a80f71b6e7ba85d24544710000d301ec
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-31 13:54:51 +0700

    event

--- notebook/static/notebook/js/cell.js
+++ notebook/static/notebook/js/cell.js
@@ -184,7 +184,7 @@ define([
      * We **might** be able to move that to notebook `handle_edit_mode`.
      */
     Cell.prototype._on_click = function (event) {
-        console.log(event.target)
+        console.log(event)
 
         if (event.target.tagName == "I" | event.target.tagName == "BUTTON")
         {
