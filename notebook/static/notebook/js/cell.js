5623e459be788685a203c491d3537a13ffd3bf2d
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-31 14:21:35 +0700

    undefined

--- notebook/static/notebook/js/cell.js
+++ notebook/static/notebook/js/cell.js
@@ -185,7 +185,7 @@ define([
      */
     Cell.prototype._on_click = function (event) {
         console.log(event.target)
-        console.log(event.target=="")
+        console.log(typeof event.target=="undefined")
         // if(event != {}){
         // if (event.target.tagName == "I" | event.target.tagName == "BUTTON")
         // {
