ba6f2c2405f323bb1063f8f3ca8698ae43397fac
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-31 10:43:06 +0700

    init notebook action

--- notebook/static/notebook/js/codecell.js
+++ notebook/static/notebook/js/codecell.js
@@ -209,7 +209,7 @@ define([
 
         var tc = $('<div></div>');
         tc.addClass("toolcell").attr('id','tool_cell');
-        var acts = new actions.init();
+        var acts = new actions.init(this.notebook);
         this.toolbar = new toolcell.ToolCell(tc, {notebook: this.notebook, actions: acts, events: events});
 
         cell.append(input).append(output).append(tc);
