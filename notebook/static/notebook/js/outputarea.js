6796bf643f305e48c344b5706f5d01d0de7d4a00
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-31 09:53:06 +0700

    remove tc

--- notebook/static/notebook/js/outputarea.js
+++ notebook/static/notebook/js/outputarea.js
@@ -99,9 +99,9 @@ define([
         
         this.prompt_overlay.addClass('out_prompt_overlay prompt');
         this.prompt_overlay.attr('title', i18n.msg._('click to expand output; double click to hide output'));
-        this.tc_dis.addClass("toolcell").attr('id','tool_cell');
-        var acts = new actions.init();
-        var toolbar = new toolcell.ToolCell('#tool_cell', {notebook: this.notebook, actions: acts, events: this.events});
+        // this.tc_dis.addClass("toolcell").attr('id','tool_cell');
+        // var acts = new actions.init();
+        // var toolbar = new toolcell.ToolCell('#tool_cell', {notebook: this.notebook, actions: acts, events: this.events});
         this.expand();
     };
 
