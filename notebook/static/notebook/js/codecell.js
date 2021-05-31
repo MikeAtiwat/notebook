96235c2e2f0cfa0d1cd0edc354955389fe0c55c2
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-05-31 17:27:56 +0700

    classlist

--- notebook/static/notebook/js/codecell.js
+++ notebook/static/notebook/js/codecell.js
@@ -461,7 +461,7 @@ define([
     // Basic cell manipulation.
 
     CodeCell.prototype.select = function () {
-        console.log(this.element)
+        console.log(this.element.classList)
         var cont = Cell.prototype.select.apply(this, arguments);
         if (cont) {
             this.code_mirror.refresh();
