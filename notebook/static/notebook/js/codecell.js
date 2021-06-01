3fed89f72153d1909652209f7577448a189b9875
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-06-01 13:55:37 +0700

    insert above below

--- notebook/static/notebook/js/codecell.js
+++ notebook/static/notebook/js/codecell.js
@@ -465,7 +465,7 @@ define([
     // Basic cell manipulation.
 
     CodeCell.prototype.select = function () {
-        console.log(this.element[0].children["tool_cell"])
+        console.log(this.element[0].children["tool_cell"].children["insert_above_below"])
         
         var cont = Cell.prototype.select.apply(this, arguments);
         if (cont) {
