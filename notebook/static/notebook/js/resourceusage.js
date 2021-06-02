f38aabaf5449c3fdce36767eb9d40f6bb04bc9d7
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-06-02 14:15:18 +0700

    px

--- notebook/static/notebook/js/resourceusage.js
+++ notebook/static/notebook/js/resourceusage.js
@@ -30,7 +30,7 @@ define([
                     .attr('title', 'Actively used Memory (updates every 5s)')
             )
                 .append(
-                    $('<span>').attr('id', 'MemProgress').append($('<div>').attr('id', 'MemBar'))
+                    $('<div>').attr('id', 'MemProgress').append($('<div>').attr('id', 'MemBar'))
                     )
                 .append(
                     $('<strong>').text(' CPU: ')
