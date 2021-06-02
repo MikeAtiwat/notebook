ebf94cc4c82754c7247a0422f7519abfd7257bb3
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-06-02 12:48:56 +0700

    width

--- notebook/static/notebook/js/resourceusage.js
+++ notebook/static/notebook/js/resourceusage.js
@@ -78,7 +78,7 @@ define([
                         var percent_use_mem = (data['rss']/limits['memory']['rss'])*100
                         console.log(percent_use_mem)
                         membar = $('#memProgress')
-                        console.log(membar)
+                        console.log(membar.style.width)
                     }
                     if (limits['memory']['warn']) {
                         $('#jupyter-resource-usage-display').addClass('jupyter-resource-usage-warn');
