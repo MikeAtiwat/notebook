6fbe39a615798dd03a935219cf6dc2b0b4f99c32
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-06-15 16:42:54 +0700

    this.

--- notebook/static/notebook/js/notebooklist.js
+++ notebook/static/notebook/js/notebooklist.js
@@ -494,7 +494,7 @@ define([
             span12.append($('<div style="margin:auto;text-align:center;color:grey"/>').text(message));
         }
         var path = this.notebook_path;
-        console.log(notebook_path)
+        console.log(this.notebook_path)
         var offset = n_uploads;
         if (path !== '') {
             item = this.new_item(offset, false);
