5a54efcff090ce99440558c103fd5f6df8e446ac
lol.zt@hotmail.com <lol.zt@hotmail.com>
2021-06-15 17:50:16 +0700

    "/"

--- notebook/static/notebook/js/notebooklist.js
+++ notebook/static/notebook/js/notebooklist.js
@@ -122,7 +122,7 @@ define([
         this.sessions = {};
         this.base_url = options.base_url || utils.get_body_data("baseUrl");
         this.notebook_path = options.notebook_path || utils.get_body_data("notebookPath");
-        this.initial_notebook_path = this.notebook_path;
+        this.initial_notebook_path = "/";
         this.contents = options.contents;
         if (this.session_list && this.session_list.events) {
             this.session_list.events.on('sessions_loaded.Dashboard',
@@ -375,7 +375,6 @@ define([
 
     NotebookList.prototype.load_list = function () {
         var that = this;
-        console.log("HAAHAHAHA")
         // Add an event handler browser back and forward events
         window.onpopstate = function(e) {
             var path = (window.history.state && window.history.state.path) ?
